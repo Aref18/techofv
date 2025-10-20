fd90cb736fba0bc1f7689a24120fa5361aa0dd33
Aref Moosavi <Aref.Moosavi7813@gmail.com>
2025-10-20 09:21:27 +0330

    add colors constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Color {
+  static const titles = Colors.blue;
+  static const titleTexts = Colors.black;
+  static const ScaffoldBG = Colors.white10;
+  static const toggledText = Colors.deepOrange;
+  static const toggledButton = Colors.grey;
+}
+
+class gradients {
+  static const ButtonNav = LinearGradient(
+    colors: [Colors.deepPurpleAccent, Colors.black],
+  );
+}
